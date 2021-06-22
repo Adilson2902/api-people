caf42d5fd150a472782882e4fdd0181377004409
Adilson2902 <38479849+Adilson2902@users.noreply.github.com>
2021-06-22 20:34:11 -0300

    up

---
+++ src/test/java/com/dio/apiperson/ApipersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.apiperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApipersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
